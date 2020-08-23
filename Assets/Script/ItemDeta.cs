fae9fd0f1439c01a94d57c7e9d6f5d4084762f68
hujishima takumi <hujishima.t425@gmail.com>
2020-08-23 21:22:48 +0900

    アイテムの基底クラスとなるItemDetaクラスを作成しました

---
+++ Assets/Script/ItemDeta.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDeta : MonoBehaviour
+{
+    public int m_cost { get; set; } = 10;
+    public int m_atackPoint { get; set; } = 5;
+    public int m_durablePoint { get; set; } = 10;
+}
