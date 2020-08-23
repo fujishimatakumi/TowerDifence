a086233ecb086c0cbff393344584df8f340f2410
hujishima takumi <hujishima.t425@gmail.com>
2020-08-23 13:47:10 +0900

    エネミーの基底クラスとなるEnemyDetaクラスを作成しました

---
+++ Assets/Script/EnemyDeta.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// エネミーの基底クラスとなる予定のデータと機能を実装するクラス
+/// </summary>
+public class EnemyDeta : MonoBehaviour
+{
+    public int m_hitPoint { get; set; }
+    public int m_atackPoint { get; set; }
+    public int m_speed { get; set; }
+}
