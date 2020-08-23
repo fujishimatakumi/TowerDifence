38d6f05aa163ce6d39a2851ff69f41abae5c748b
hujishima takumi <hujishima.t425@gmail.com>
2020-08-23 13:38:16 +0900

    プレイヤーの基底クラスとなるPlayerDetaクラスを作成しました

---
+++ Assets/Script/PlayerDeta.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// プレイヤーに必要なデータと機能を持つ基底クラスになる予定のクラス
+/// </summary>
+public class PlayerDeta : MonoBehaviour
+{
+    public int m_hitPoint { get; set; } = 50;
+    public int m_atackPoint { get; set; } = 5;
+    public int m_speedPoint { get; set; } = 10;
+}
