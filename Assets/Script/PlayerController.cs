3bb52e0c817e3b83ac6e641ea24e020b09b2625f
hujishima takumi <hujishima.t425@gmail.com>
2020-09-01 16:32:08 +0900

    EnemyShotメソッドを追加しました

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //プレイヤーの移動速度
     float m_speed = 1f;
+    //プレイヤーが移動する位置
     Transform m_moveTarget;
+    //攻撃対象
     GameObject m_target;
-    [SerializeField]LayerMask m_hitLayer;
+    [SerializeField] LayerMask m_hitLayer;
+    //レイを飛ばす距離
     [SerializeField] float m_distance;
+    //攻撃の間隔
+    [SerializeField] float m_attackTime = 2f;
+    //弾となるプレハブ
+    [SerializeField] GameObject m_shotPrefab;
+    float m_attackTimeCount;
     // Start is called before the first frame update
     void Start()
     {
         m_moveTarget = null;
         m_target = null;
+        m_attackTimeCount = m_attackTime;
     }
 
     // Update is called once per frame
@@ -29,8 +39,21 @@ public class PlayerController : MonoBehaviour
         }
         if (m_target)
         {
+            if (m_attackTimeCount <= 0)
+            {
+                EnemyShot();
+            }
+            else
+            {
+                m_attackTime -= Time.deltaTime;
+            }
             Debug.Log("TargetName = " + m_target.name);
             Debug.Log("攻撃中");
+        }
+        //ターゲットが外れた場合発射間隔カウントを初期化する
+        else
+        {
+            m_attackTimeCount = m_attackTime;
         }
     }
 
@@ -38,12 +61,13 @@ public class PlayerController : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, m_distance, m_hitLayer);
-        //敵をクリックした場合ターゲットを設定する
+        //それ以外をクリックした場合設定した項目を設定から外す
         if (hit.collider == null)
         {
             m_target = null;
             m_moveTarget = null;
         }
+        //敵をクリックした場合ターゲットを設定する
         else if (hit.collider.tag == "Enemy")
         {
             m_target = hit.collider.gameObject;
@@ -57,7 +81,9 @@ public class PlayerController : MonoBehaviour
                 m_target = null;
             }
         }
-        //それ以外をクリックした場合設定した項目を設定から外す
-        
+    }
+    void EnemyShot()
+    {
+        Instantiate(m_shotPrefab, this.gameObject.transform.position, Quaternion.identity);
     }
 }
