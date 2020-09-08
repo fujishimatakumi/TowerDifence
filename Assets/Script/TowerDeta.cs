69b8bee51c1491067bc0ba2dcf1cc7b06eeaac42
hujishima takumi <hujishima.t425@gmail.com>
2020-09-08 17:09:20 +0900

    TowerDetaスクリプトを作成しました タワーに必要な機能等が入っています

---
+++ Assets/Script/TowerDeta.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDeta : MonoBehaviour
+{
+    [SerializeField] int m_towerHP = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DamageToTower(int damage)
+    {
+        m_towerHP -= damage;    
+    }
+}
