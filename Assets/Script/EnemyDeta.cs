96e3751f6aa7818411143d223d34a5686ff01fe6
hujishima takumi <hujishima.t425@gmail.com>
2020-09-08 17:01:34 +0900

    EnemyDetaスクリプトにDamage関数を追加しました

--- Assets/Script/EnemyDeta.cs
+++ Assets/Script/EnemyDeta.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// エネミーの基底クラスとなる予定のデータと機能を実装するクラス
+/// エネミーの機能を実装
 /// </summary>
 public class EnemyDeta : MonoBehaviour
 {
-    public int m_hitPoint { get; set; }
+    [field:SerializeField]
+    public int m_hitPoint { get;private set; }
+    [field:SerializeField]
     public int m_atackPoint { get; set; }
-    public int m_speed { get; set; }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        string tag = collision.gameObject.tag;
+        switch (tag)
+        {
+            case "tower":
+                Debug.Log("タワーが攻撃されました");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Damage(int damege)
+    {
+        m_hitPoint -= damege;
+    }
 }
