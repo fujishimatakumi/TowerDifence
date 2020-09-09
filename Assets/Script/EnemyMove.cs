dbbe7cfc4bd0356aa16db421fe63d7a786689808
hujishima takumi <hujishima.t425@gmail.com>
2020-09-09 11:20:23 +0900

    waypointを持ったオブジェクトを名前でシーンから見つけてくるようにしました

--- Assets/Script/EnemyMove.cs
+++ Assets/Script/EnemyMove.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    [SerializeField] GameObject m_wayPointsObject;
+    [SerializeField] string m_wayPointsObject = "WayPoint1";
     WayPoint waypoints;
     public float Speed = 1f;
     int wayPointIndex;
     // Start is called before the first frame update
     void Start()
     {
-        waypoints = m_wayPointsObject.GetComponent<WayPoint>();
+        GameObject waypointObj = GameObject.Find(m_wayPointsObject);
+        waypoints = waypointObj.GetComponent<WayPoint>();
     }
 
     // Update is called once per frame
@@ -19,17 +20,18 @@ public class EnemyMove : MonoBehaviour
     {
         transform.position = Vector2.MoveTowards(transform.position, waypoints.m_wayPoints[wayPointIndex].position, Speed * Time.deltaTime);
 
-        /*実装方法１
-         * キャラクターが前を向くが計算式を理解していない
+        //実装方法１
+        //キャラクターが前を向くが計算式を理解していない
         Vector3 dir = waypoints.m_wayPoints[wayPointIndex].position - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        */
+        
+        /*
         //実装方法２
         //正規化して関数で向かせる方法
         Vector3 dir = (waypoints.m_wayPoints[wayPointIndex].position - gameObject.transform.position).normalized;
         gameObject.transform.rotation = Quaternion.FromToRotation(Vector3.right, dir);
-        
+        */
         if (Vector2.Distance(transform.position,waypoints.m_wayPoints[wayPointIndex].position) < 0.1f)
         {
             if (wayPointIndex < waypoints.m_wayPoints.Length - 1)
