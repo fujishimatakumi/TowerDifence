44b0fe13efc8b1e7b846d5382097129395cb3499
hujishima takumi <hujishima.t425@gmail.com>
2020-09-25 16:32:00 +0900

    Move関数を作りましたエネミーの動きをアップデートからコルーチンに変えました

--- Assets/Script/EnemyMove.cs
+++ Assets/Script/EnemyMove.cs
@@ -13,35 +13,45 @@ public class EnemyMove : MonoBehaviour
     {
         GameObject waypointObj = GameObject.Find(m_wayPointsObject);
         waypoints = waypointObj.GetComponent<WayPoint>();
+        StartCoroutine(Move(waypoints));
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, waypoints.m_wayPoints[wayPointIndex].position, Speed * Time.deltaTime);
+       
+    }
 
-        //実装方法１
-        //キャラクターが前を向くが計算式を理解していない
-        Vector3 dir = waypoints.m_wayPoints[wayPointIndex].position - transform.position;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        
-        /*
-        //実装方法２
-        //正規化して関数で向かせる方法
-        Vector3 dir = (waypoints.m_wayPoints[wayPointIndex].position - gameObject.transform.position).normalized;
-        gameObject.transform.rotation = Quaternion.FromToRotation(Vector3.right, dir);
-        */
-        if (Vector2.Distance(transform.position,waypoints.m_wayPoints[wayPointIndex].position) < 0.1f)
+    public IEnumerator Move(WayPoint wayPoint)
+    {
+        while (wayPointIndex < wayPoint.m_wayPoints.Length)
         {
-            if (wayPointIndex < waypoints.m_wayPoints.Length - 1)
-            {
-                wayPointIndex++;
-            }
-            else
-            { 
-            }
-        }
+            transform.position = Vector2.MoveTowards(transform.position, waypoints.m_wayPoints[wayPointIndex].position, Speed * Time.deltaTime);
 
+            //実装方法１
+            //キャラクターが前を向くが計算式を理解していない
+            Vector3 dir = waypoints.m_wayPoints[wayPointIndex].position - transform.position;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            /*
+            //実装方法２
+            //正規化して関数で向かせる方法
+            Vector3 dir = (waypoints.m_wayPoints[wayPointIndex].position - gameObject.transform.position).normalized;
+            gameObject.transform.rotation = Quaternion.FromToRotation(Vector3.right, dir);
+            */
+            if (Vector2.Distance(transform.position, waypoints.m_wayPoints[wayPointIndex].position) < 0.1f)
+            {
+                if (wayPointIndex < waypoints.m_wayPoints.Length - 1)
+                {
+                    wayPointIndex++;
+                }
+                else
+                {
+                }
+            }
+
+            yield return null;
+        }
     }
 }
