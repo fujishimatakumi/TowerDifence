8f3565e0ce929c375e17790fe48c209c181216cc
hujishima takumi <hujishima.t425@gmail.com>
2020-09-26 19:36:35 +0900

    トラップをドラッグアンドドロップで設置するためのスクリプトを作成しました

---
+++ Assets/Script/TrapDragController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TrapDragController : MonoBehaviour,IDragHandler,IDropHandler,IBeginDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        RectTransform canvasTranse = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        GameObject go = GameObject.Instantiate(this.gameObject) as GameObject;
+        go.transform.SetParent(canvasTranse);
+        go.transform.localPosition = this.gameObject.transform.localPosition;
+        go.transform.localScale = this.gameObject.transform.localScale;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    { 
+        this.gameObject.transform.position = eventData.position;
+    }
+    
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Destroy(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
