eae4ae884d51a9aaec29d63cb8b348f57a5d0201
hujishima takumi <hujishima.t425@gmail.com>
2020-09-26 21:14:56 +0900

    setFieldタグを追加しました ドラッグアンドドロップでトラップを設置できるようにしました（バグ修正済み）

--- Assets/Script/TrapDragController.cs
+++ Assets/Script/TrapDragController.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
 
 public class TrapDragController : MonoBehaviour,IDragHandler,IDropHandler,IBeginDragHandler
 {
+    [SerializeField] float m_distance = 10f;
+    [SerializeField] LayerMask m_hitLayer;
+    [SerializeField] GameObject m_setObject;
+    GameManager m_gameManager;
+    TrapDeta m_objectData;
+    Tilemap m_tilemap;
+
+    private void Start()
+    {
+        m_gameManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManager>();
+        m_objectData = m_setObject.GetComponent<TrapDeta>();
+        m_tilemap = GameObject.FindGameObjectWithTag("setField").GetComponent<Tilemap>();
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
         RectTransform canvasTranse = GameObject.Find("Canvas").GetComponent<RectTransform>();
@@ -23,18 +37,37 @@ public class TrapDragController : MonoBehaviour,IDragHandler,IDropHandler,IBegin
 
     public void OnDrop(PointerEventData eventData)
     {
+        TrapInstallation();
         Destroy(this.gameObject);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void TrapInstallation()
     {
+        
+            /*
+            Vector3 screenPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 setPos = screenPos + new Vector3(0, 0, 10);
+            */
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D[] hits = Physics2D.RaycastAll(ray.origin, ray.direction, m_distance, m_hitLayer);
+        
+        foreach (var item in hits)
+        {
+            if (item.collider.gameObject.tag == "Trap") return;
+            if (item.collider.gameObject.tag == "setField")
+            {
+                if (m_gameManager.m_resourcePoint >= m_objectData.m_cost)
+                {
+                    var tilepos = m_tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                    Debug.Log(tilepos);
+                    Vector3 setPos = m_tilemap.GetCellCenterWorld(tilepos);
+                    Instantiate(m_setObject, setPos, Quaternion.identity);
+                    m_gameManager.SubtractResourcePoint(m_objectData.m_cost);
+                }
+            }
+        }
+            
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
