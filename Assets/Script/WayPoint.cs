cd6d861cddeb658eed69fd208de1b03b8406adf5
hujishima takumi <hujishima.t425@gmail.com>
2020-09-28 10:33:34 +0900

    WaypointがタイルマップのセンターになるようにするSetToTilemapCenterWorldPoint関数を追加しました

--- Assets/Script/WayPoint.cs
+++ Assets/Script/WayPoint.cs
@@ -1,10 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class WayPoint : MonoBehaviour
 {
     //オブジェクトが移動するためのパス
     public Transform[] m_wayPoints;
+
+    [SerializeField] string m_tagToTilemap = "setField";
+
+    private void Start()
+    {
+        SetToTilemapCenterWorldPoint();
+    }
+    public void SetToTilemapCenterWorldPoint()
+    {
+        Tilemap tm = GameObject.FindGameObjectWithTag(m_tagToTilemap).GetComponent<Tilemap>();
+        for (int i = 0; i < m_wayPoints.Length; i++)
+        {
+            var tilepos = tm.WorldToCell(m_wayPoints[i].position);
+            Vector3 center = tm.GetCellCenterWorld(tilepos);
+            m_wayPoints[i].position = center;
+        }
+    }
     
 }
