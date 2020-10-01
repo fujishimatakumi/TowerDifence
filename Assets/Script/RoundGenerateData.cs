275f7e9be9ee280b89f1e31b743fb6f45aeb8970
hujishima takumi <hujishima.t425@gmail.com>
2020-10-01 14:38:08 +0900

    ラウンドごとのエネミージェネレイトデータを保持するRoundGenerateDataクラスを作成しました

---
+++ Assets/Script/RoundGenerateData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundGenerateData : MonoBehaviour
+{
+    public GenerateData[] m_generatDatas;
+    [field:SerializeField]
+    public float NextWaiteTime { get; private set; }
+}
